7d83a399a454306a377b325ab491206c17888364
JohnGrib <johngrib82@gmail.com>
2022-10-25 22:58:38 +0900

    AOC 2020 4-2 with spec 코드 정리

--- src/aoc2020/problem_4_2_with_spec.clj
+++ src/aoc2020/problem_4_2_with_spec.clj
@@ -1,44 +1,44 @@
 (ns aoc2020.problem-4-2-with-spec
-  (:require [aoc2020.problem-4-1 :refer :all]
-            [aoc2020.problem-4-2 :refer :all]
+  (:require [aoc2020.problem-4-1 :refer [sample-input-string input-string string->passport]]
+            [aoc2020.problem-4-2 :refer [number-string? valid-height? invalid-strings valid-strings]]
             [clojure.spec.alpha :as s]
             [clojure.string :as str]))
 
 (s/def :passport/byr (s/and
-                       some?
-                       number-string?
-                       #(<= 1920 (Integer/parseInt %) 2002)))
+                      some?
+                      number-string?
+                      #(<= 1920 (Integer/parseInt %) 2002)))
 (s/def :passport/iyr (s/and
-                       some?
-                       number-string?
-                       #(<= 2010 (Integer/parseInt %) 2020)))
+                      some?
+                      number-string?
+                      #(<= 2010 (Integer/parseInt %) 2020)))
 (s/def :passport/eyr (s/and
-                       some?
-                       number-string?
-                       #(<= 2020 (Integer/parseInt %) 2030)))
+                      some?
+                      number-string?
+                      #(<= 2020 (Integer/parseInt %) 2030)))
 (s/def :passport/hgt (s/and
-                       some?
-                       valid-height?))
+                      some?
+                      valid-height?))
 (s/def :passport/hcl (s/and
-                       some?
-                       #(re-matches #"^#[0-9a-f]{6}$" %)))
+                      some?
+                      #(re-matches #"^#[0-9a-f]{6}$" %)))
 (s/def :passport/ecl (s/and
-                       some?
-                       #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"}))
+                      some?
+                      #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"}))
 (s/def :passport/pid (s/and
-                       some?
-                       number-string?
-                       #(re-matches #"^\d{9}$" %)))
+                      some?
+                      number-string?
+                      #(re-matches #"^\d{9}$" %)))
 (s/def :passport/cid (constantly true))
 
 (s/def ::passport (s/keys :req-un [:passport/byr
-                                      :passport/iyr
-                                      :passport/eyr
-                                      :passport/hgt
-                                      :passport/hcl
-                                      :passport/ecl
-                                      :passport/pid]
-                             :opt-un [:passport/cid]))
+                                   :passport/iyr
+                                   :passport/eyr
+                                   :passport/hgt
+                                   :passport/hcl
+                                   :passport/ecl
+                                   :passport/pid]
+                          :opt-un [:passport/cid]))
 
 (defn solve-4-2-with-spec
   "https://adventofcode.com/2020/day/4 part 2 문제를 풀이하여, 유효한 여권의 수를 리턴합니다."
@@ -53,4 +53,5 @@
   (solve-4-2-with-spec valid-strings)                       ; 4
   (solve-4-2-with-spec sample-input-string)                 ; 2
   (solve-4-2-with-spec input-string)                        ; 184
+  ;;
   )
